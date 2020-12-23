d96e4835d899abe39aff599f6f1517eb07f3fca8
LazerSagittarius <48451527+LazerSagittarius@users.noreply.github.com>
2020-12-23 10:40:33 +0100

    Add files via upload

---
+++ RGB2Grey.m
@@ -0,0 +1,35 @@
+function [greyImage] = RGB2Grey(imageName)
+% takes an rgb image and returns a grey image
+
+% image must be present in folder Frames
+% image must be a .jpg
+
+filePath = strcat('Frames/', imageName, '.jpg');
+
+if 2==exist(filePath,'file')
+    % File exists.
+    
+    image = imread(filePath);
+    %extrag color channels and convert to double
+    r = image(:,:,1);
+    g = image(:,:,2);
+    b = image(:,:,3);
+    r = double(r);
+    g = double(g);
+    b = double(b); 
+
+    %find grey value for coresponding rgb-values
+    greyImage = r+g+b;
+    greyImage = greyImage/3
+    greyImage = greyImage/255  
+
+else 
+    % File does not exist.
+     disp('RGB2Grey: error, no such file exists');
+
+end
+
+
+
+
+
---
+++ Video2Frames.m
@@ -0,0 +1,27 @@
+function Video2Frames(fileName)
+% takes the name of a video and processes it. Function will save each frame 
+% of the given video as a .jpg in the folder "Frames"
+
+% Vorbedingungen: 
+% folders "VideoFiles" & "Frames" need to be present in working directory
+% video file must be .mp4 and file must be present in "VideoFiles"
+
+
+filePath = strcat('VideoFiles/', fileName, '.mp4');
+if 2==exist(filePath,'file')
+    % File exists.
+    
+    video = VideoReader(filePath); 
+    
+    % iterating each frame and writing it to Frames/ as framex.jpg
+    for img = 1:video.NumberOfFrames
+        filename = strcat('Frames/frame', num2str(img), '.jpg');
+        tempframe = read(video, img); 
+        imwrite(tempframe, filename);
+    end
+
+else 
+    % File does not exist.
+     disp('Video2Frames: error, no such file exists');
+end
+
