9c4c0a912b4f0b3d60e6796eaf093d28bd01804c
baurkevin <45953547+baurkevin@users.noreply.github.com>
2020-12-28 16:32:59 +0100

    Update RGB2Grey.m
    added code with works with my code

--- RGB2Grey.m
+++ RGB2Grey.m
@@ -31,3 +31,26 @@ else
      disp('RGB2Grey: error, no such file exists');
 
 end
+
+
+%%kevins version
+
+function [greyImage] = RGB2Grey(image)
+% takes an rgb image and returns a grey image
+% takes the name of an image as a string!
+   
+    %extrag color channels and convert to double
+    r = image(:,:,1);
+    g = image(:,:,2);
+    b = image(:,:,3);
+    r = double(r);
+    g = double(g);
+    b = double(b); 
+
+    %find grey value for coresponding rgb-values
+    greyImage = r+g+b;
+    greyImage = greyImage/3;
+    greyImage = uint8(greyImage);
+    % greyImage = greyImage/255  
+
+end
