19f15d6b088e20f7e2c80c06b964e19fa84cecaa
baurkevin <45953547+baurkevin@users.noreply.github.com>
2020-12-28 16:36:35 +0100

    Update Grey2Binary.m
    deleted kevins version

--- Grey2Binary.m
+++ Grey2Binary.m
@@ -26,32 +26,3 @@ inputImg(inputImg >= k) = 1;
 binaryImage = double(inputImg);
 end
 
-
-%%kevins version
-function [binaryImage] = Grey2Binary(inputImg)
-% this function uses otsu-thresholding with N = 2 classes (fixed)
-% e.g.: newImg = Grey2Binary(testImage);
-
-% k is the ideal threshold
-
-% image must be greyscale!
-
-% compute k = ideal threshold using a histogram 
-counts = imhist(inputImg);
-L = length(counts);
-p = counts / sum(counts);
-omega = cumsum(p);
-mu = cumsum(p .* (1:L)');
-mu_t = mu(end);
-
-sigma_b_squared = (mu_t * omega - mu).^2 ./ (omega .* (1 - omega));
-
-[~,k] = max(sigma_b_squared);
-
-% convert image to binary using k as threshold
-inputImg(inputImg < k) = 0; 
-inputImg(inputImg >= k) = 1; 
-
-% create output while converting to double
-binaryImage = double(inputImg);
-end
