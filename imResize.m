1de87155f6e1740e8c31c31310542e61363b81a5
baurkevin <45953547+baurkevin@users.noreply.github.com>
2020-12-31 13:26:48 +0100

    Update imResize.m

--- imResize.m
+++ imResize.m
@@ -1,20 +1,17 @@
-function [outputImage] = imResize(im, scalerow, scalecolum)
-%   Detailed explanation goes here
+%Kevin Baur 11827180
+function [outputImage] = imResize(inputImage, scalerow, scalecolum)
+%imResize resizes the image on given rows and scales
 
-%initializations:
-inputImage = im;
-scale = [scalerow scalecolum];                                %scalefactor [rows colums]
-oldSize = size(inputImage);                   %size of your image
+%initializations
+scale = [scalerow scalecolum];                %scalefactor [rows colums]
+oldSize = size(inputImage);                   %size of the image
 newSize = max(floor(scale.*oldSize(1:2)),1);  %compute the new image size
 
-%upsampled set of indices:
-
+%upsampled set of indices
 rowIndex = min(round(((1:newSize(1))-0.5)./scale(1)+0.5),oldSize(1));
 colIndex = min(round(((1:newSize(2))-0.5)./scale(2)+0.5),oldSize(2));
 
-%index old image to get new image:
+%index old image to get new image
 outputImage = inputImage(rowIndex,colIndex,:);
-
-imshow(outputImage);
-
 end
+
