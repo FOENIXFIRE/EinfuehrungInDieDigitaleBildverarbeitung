b4602034b154f5ec0925d519495b0c2a52558ed9
baurkevin <45953547+baurkevin@users.noreply.github.com>
2020-12-31 13:26:17 +0100

    Update imDilation.m

--- imDilation.m
+++ imDilation.m
@@ -1,19 +1,27 @@
-function [im] = imDilation(im, strEl, R)
-%imDilation Summary of this function goes here
-%   Detailed explanation goes here
+%Kevin Baur 11827180
+function [dilatedImage] = imDilation(im, strEl, R)
+%imDilation expands the shapes contained in the input image
+%  dilation operation adds pixels to the boundaries of objects of an
+%  image. In a binary image,a pixel is set to 1 if any of the neighboring 
+%  pixels have the value 1.
+%  Morphological dilation makes objects more visible and fills in small holes in objects.
+
+%r is the size of the doubled struct element
 r = R * 2;
-A=im;
+
 %Structuring element
-B = strEl;
-%Pad zeros on all the sides
-C=padarray(A,[1 1]);
-%Intialize a matrix of matrix size A with zeros
-D=false(size(A));
-for i=1:size(C,1)-r
-    for j=1:size(C,2)-r
-        %Perform logical AND operation
-        D(i,j)=sum(sum(B&C(i:i+r,j:j+r)));
+struct = strEl;
+
+%Pad array on all the sides to use struct element accordingly
+padA=padarray(im,[1 1]);
+
+%Intialize a matrix of matrix size im with zeros
+result=false(size(im));
+for i=1:size(padA,1)-r
+    for j=1:size(padA,2)-r
+        %logical AND operation each pixel
+        result(i,j) = sum(sum(struct&padA(i:i+r,j:j+r)));
     end
 end
-im = D;
+dilatedImage = result;
 end
