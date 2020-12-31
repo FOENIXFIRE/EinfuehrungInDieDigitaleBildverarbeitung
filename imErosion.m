f00d4b353e37af86f97f4f57101b35deac397235
baurkevin <45953547+baurkevin@users.noreply.github.com>
2020-12-31 13:25:46 +0100

    Update imErosion.m

--- imErosion.m
+++ imErosion.m
@@ -1,28 +1,30 @@
-function [im] = imErosion(im)
-%imErosion Summary of this function goes here
-%   Detailed explanation goes here
-A = im;
+%Kevin Baur 11827180
+function [erodedImage] = imErosion(im)
+%imErosion reduces the shape of objects in a given image
+%  dilation operation removes pixels of the boundaries of objects of an
+%  image.
+%  In a binary image, a pixel is set to 0 if any of the neighboring pixels
+%  have the value 0.
+%  Morphological erosion removes islands and small objects so that only substantive objects remain.
 
 %Structuring element
-B = getnhood(strel('diamond',10));
+struct = getnhood(strel('diamond',10));
 
-m=floor(size(B,1)/2);
-n=floor(size(B,2)/2);
-%Pad array on all the sides
-C=padarray(A,[m n],1);
+%round
+m=floor(size(struct,1)/2);
+n=floor(size(struct,2)/2);
 
-%Pad array with ones on both sides
-%%C = padarray(A,[0 1],1);
-%Intialize the matrix D of size A with zeros
-D = false(size(A));
-for i=1:size(C,1)-(2*m)
-    for j=1:size(C,2)-(2*n)
-       
-        Temp=C(i:i+(2*m),j:j+(2*n));
-       
-        D(i,j)=min(min(Temp));
+%Pad array on all the sides to use struct element accordingly
+padA = padarray(im,[m n],1);
+
+%Intialize the matrix result of size im with zeros
+result = false(size(im));
+
+for i=1:size(padA,1)-(2*m)
+    for j=1:size(padA,2)-(2*n) 
+       Temp = padA(i:i+(2*m),j:j+(2*n));
+       result(i,j) = min(min(Temp));
     end
 end
-im = D;
+erodedImage = result;
 end
-
