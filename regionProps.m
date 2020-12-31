f7bf7b8bbbd47f1a3521ce445ccfcdf5be35a4f6
baurkevin <45953547+baurkevin@users.noreply.github.com>
2020-12-31 13:23:18 +0100

    Update regionProps.m

--- regionProps.m
+++ regionProps.m
@@ -1,41 +1,38 @@
-function  [found, foundBBox, foundCentroid] = regionProps()
-%regionProps Summary of this function goes here
-%   Detailed explanation goes here
-
-%Load image
-I=imread('suzuki.png');
-
-%Convert to Gray
-I= rgb2gray(I);
-
-%Convert to Binary
-B=imbinarize(I);
-                                                 
-%Fill the holes
-C=imfill(B,'holes');
+%Kevin Baur 11827180
+function  [foundBBox] = regionProps(inputImage)
+%regionProps labels the filled surfaces, sets a boundingBox around them
+%and returns the boundigBox of the logo
+%   with labelling every surface gets a number, then every labelled surface
+%   gets surrounded with a boundingBox. The best matching boundingBox gets
+%   returned and it will be the logo of the car.
+ 
+%input image                                                
+image = inputImage;
                                                    
- %Label the image
-[Label,Total]=bwlabel(C,8);
+%label the image
+[Label,Total]=bwlabel(image,8);  %Total = labelled surfaces
 
-imshow(C); 
+imshow(image); 
+
+%goes throught all labelled surfaces and calculates every BoundingBox, then
+%checks if its the logo with object_area and position in the inputImage
 for num=1:Total
     [row, col] = find(Label==num);
     
-    %Bounding Box
+    %To find Bounding Box
     sx=min(col)-0.5;
     sy=min(row)-0.5;
     breadth=max(col)-min(col)+1;
     len=max(row)-min(row)+1;
     BBox=[sx sy breadth len];
-    display(BBox);
-    %figure,imshow(I);
+    display(BBox);   
     hold on;
     x=zeros([1 5]);
     y=zeros([1 5]);
-    x(:)=BBox(1);
-    y(:)=BBox(2);
-    x(2:3)=BBox(1)+BBox(3);
-    y(3:4)=BBox(2)+BBox(4);
+    x(:)=BBox(1); %x
+    y(:)=BBox(2); %y
+    x(2:3)=BBox(1)+BBox(3); %breatdth
+    y(3:4)=BBox(2)+BBox(4); %len
     plot(x,y);
 
     %Find Area
@@ -49,13 +46,17 @@ for num=1:Total
     plot(X,Y,'ro','color','r');
     hold off;
 
-    %find logo BoundingBox
-    if X > 200 & Y < 100 & X < 300 & Y > 10
-        if Obj_area > 1000 & Obj_area < 7000
+    %checking if it could be logo
+    if X > 200 & Y < 300 & X < 700 & Y > 10
+        if Obj_area > 300 & Obj_area < 7000
             found = num;
-            foundBBox = BBox;
-            foundCentroid = Centroid;
+            %make boundingBox larger for better crop
+            BBox(1) = BBox(1)-10;
+            BBox(2) = BBox(2)-5;
+            BBox(3) = BBox(3)+25;
+            BBox(4) = BBox(4)+20;
+            foundBBox = BBox;           %BBox of logo
+            foundCentroid = Centroid;   %Centroid of the logo
         end
     end
-    
 end
