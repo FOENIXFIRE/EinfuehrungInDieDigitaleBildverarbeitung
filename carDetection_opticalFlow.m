e202874a6a716b294f5535f4d45cf816c6830b47
Vincent Zöchling <vinc.z@gmx.at>
2021-01-06 19:02:21 +0100

    Changed: carDet_opt

--- carDetection_opticalFlow.m
+++ carDetection_opticalFlow.m
@@ -1,18 +1,22 @@
 function carDetection_opticalFlow (videoName)
 
-clear;
 close all;
 clc;
 
 %%Get Frames
-Video2Frames("hyundai")
+Video2Frames(videoName)
 
 
-h = figure;
-        movegui(h);
-        hViewPanel = uipanel(h,'Position',[0 0 1 1],'Title','Plot of Optical Flow Vectors');
+opticalVectorPlot = figure;
+        movegui(opticalVectorPlot);
+        hViewPanel = uipanel(opticalVectorPlot,'Position',[0 0 1 1],'Title','Plot of Optical Flow Vectors');
         hPlot = axes(hViewPanel);
 
+threshHoldPlot = figure;  
+
+originalImgPlot = figure;
+        
+        
 opticFlow = opticalFlowHS;
 
     for count = 1:3:(numel(dir("Frames"))-2)
@@ -22,22 +26,30 @@ opticFlow = opticalFlowHS;
         img1resize = imResize(imgFile, 0.2, 0.2);
         img1gray = RGB2Grey(img1resize);
         
-        IThresh = threshholding(img1gray, 130);
+       
+        
+        set(0, 'CurrentFigure', originalImgPlot)
+        imshow(img1resize);
         
         
-%         imshow(imgFile);
+        set(0, 'CurrentFigure', opticalVectorPlot)
         flowField = estimateFlow(opticFlow,img1gray);
-%         hold on
-%         plot(flowField,'DecimationFactor',[10 10],'ScaleFactor',20,'Parent',hPlot);
-%         title('OPtical Flow');
-%         hold off
-%         pause (10^-3)
+        imshow(img1resize);
+        hold on
+        plot(flowField,'DecimationFactor',[5 5],'ScaleFactor',30,'Parent',hPlot);
+        title('Optical Flow');
+        hold off
+        pause (10^-3)
+        
+        
         
         imgMag = flowField.Magnitude;
         imgMagThr = threshholding(imgMag, mean(imgMag(:)));
         imgEro = imErosion(imgMagThr,6);
 %       imgEro = imErosion(imgMagThr);
-        imshow(imgEro);
+
+        set(0, 'CurrentFigure', threshHoldPlot)
+          imshow(imgEro);
         
         
         
