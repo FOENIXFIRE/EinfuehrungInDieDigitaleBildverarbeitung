9cbdf37a1ca9302f1489e7dff8039df1d006ae82
LazerSagittarius <48451527+LazerSagittarius@users.noreply.github.com>
2021-01-06 10:48:17 +0100

    Update Grey2Binary.m

--- Grey2Binary.m
+++ Grey2Binary.m
@@ -1,28 +1,30 @@
-function [binaryImage] = Grey2Binary(inputImg)
-% this function uses otsu-thresholding with N = 2 classes (fixed)
-% e.g.: newImg = Grey2Binary(testImage);
+function [binaryImage] = Grey2Binary(input)
+% takes an greyscale image as input 
+% returns binary image by using otsu-thresholding with 2 classes (fixed)
+% 
+% e.g.: binaryImage = Grey2Binary(input);
+%
+% source: https://blogs.mathworks.com/steve/2016/06/14/image-binarization-otsus-method/?from=jp
 
-% k is the ideal threshold
 
-% image must be greyscale!
-
-% compute k = ideal threshold using a histogram 
-counts = imhist(inputImg);
-L = length(counts);
-p = counts / sum(counts);
+% compute ideal threshold using a histogram 
+x = MyHist(input);
+histLength = length(x);
+p = x / sum(x);
 omega = cumsum(p);
-mu = cumsum(p .* (1:L)');
+mu = cumsum(p .* (1:histLength)');
 mu_t = mu(end);
 
+% maximizing sigma_b_squared (as otsu suggests)
 sigma_b_squared = (mu_t * omega - mu).^2 ./ (omega .* (1 - omega));
-
-[~,k] = max(sigma_b_squared);
+% chosing the highest such value as ideal k for the threshold
+[~,threshold] = max(sigma_b_squared);
 
 % convert image to binary using k as threshold
-inputImg(inputImg < k) = 0; 
-inputImg(inputImg >= k) = 1; 
+input(input < threshold) = 0; 
+input(input >= threshold) = 1; 
 
-% create output while converting to double
-binaryImage = double(inputImg);
+% create output and convert to double
+binaryImage = double(input);
 end
 
