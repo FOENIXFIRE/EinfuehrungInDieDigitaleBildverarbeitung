148b0aebeba83524f413f1e502f11dc2862476dd
LazerSagittarius <48451527+LazerSagittarius@users.noreply.github.com>
2021-01-06 16:39:14 +0100

    Update MyHist.m

--- MyHist.m
+++ MyHist.m
@@ -1,6 +1,6 @@
 % Jan König (01007167)
 function [hist] = MyHist(input)
-% takes an greyscale image as input 
+% takes a greyscale image as input 
 % returns histogram of image
 % 
 % e.g.: hist = MyHist(input);
@@ -10,14 +10,18 @@ function [hist] = MyHist(input)
 % create double array 256x1
 hist = zeros(256,1);
 
-% iterate over each pixel and fill array
+% get size of image/array for iteration
 [row col] = size(input);
 
+% iterate over each pixel
 for i=1:row
     for j=1:col
-        temp = input(i,j);   
+        temp = input(i,j);
+        
+        % this is needed due to matlab starting at index 1 but values for array starting at 0
         temp = temp+1;
-            
+        
+        % for each value found in image/array raise the appropriate counter by 1
         hist(temp) = hist(temp)+1;
     end
 end
