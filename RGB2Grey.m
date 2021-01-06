dc5a092e62f47bc15665141bc56c8e1df4450147
LazerSagittarius <48451527+LazerSagittarius@users.noreply.github.com>
2021-01-06 11:12:51 +0100

    Update RGB2Grey.m

--- RGB2Grey.m
+++ RGB2Grey.m
@@ -1,56 +1,23 @@
-function [greyImage] = RGB2Grey(imageName)
+function [greyImage] = RGB2Grey(input)
 % takes an rgb image and returns a grey image
-% takes the name of an image as a string!
-% e.g.: newImg = RGB2Grey('testImage');
-
+% e.g.: greyImage = RGB2Grey(input);
+%
 % image must be present in folder Frames
 % image must be a .jpg
-
-filePath = strcat('Frames/', imageName, '.jpg');
-
-if 2==exist(filePath,'file')
-    % File exists.
+%
+% source: none needed
     
-    image = imread(filePath);
-    %extrag color channels and convert to double
-    r = image(:,:,1);
-    g = image(:,:,2);
-    b = image(:,:,3);
-    r = double(r);
-    g = double(g);
-    b = double(b); 
+% split into seperate color channels and convert each to double
+r = input(:,:,1);
+g = input(:,:,2);
+b = input(:,:,3);
+r = double(r);
+g = double(g);
+b = double(b); 
 
-    %find grey value for coresponding rgb-values
-    greyImage = r+g+b;
-    greyImage = greyImage/3;
-    greyImage = uint8(greyImage);
-    % greyImage = greyImage/255  
-
-else 
-    % File does not exist.
-     disp('RGB2Grey: error, no such file exists');
-
-end
-
-
-%%kevins version
-
-function [greyImage] = RGB2Grey(image)
-% takes an rgb image and returns a grey image
-% takes the name of an image as a string!
-   
-    %extrag color channels and convert to double
-    r = image(:,:,1);
-    g = image(:,:,2);
-    b = image(:,:,3);
-    r = double(r);
-    g = double(g);
-    b = double(b); 
-
-    %find grey value for coresponding rgb-values
-    greyImage = r+g+b;
-    greyImage = greyImage/3;
-    greyImage = uint8(greyImage);
-    % greyImage = greyImage/255  
+% determine grey value for coresponding rgb-values
+greyImage = r+g+b;
+greyImage = greyImage/3;
+greyImage = uint8(greyImage);
 
 end
