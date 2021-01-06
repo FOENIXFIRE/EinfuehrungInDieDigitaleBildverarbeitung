21975d732ae218ed3615e9ab46724a732fcb6c21
baurkevin <45953547+baurkevin@users.noreply.github.com>
2021-01-06 19:45:54 +0100

    Update positionLogo.m

--- positionLogo.m
+++ positionLogo.m
@@ -1,35 +1,32 @@
 %Kevin Baur 11827180
-function [croppedImage] = logo_position()
+function [croppedImage] = logoPosition(inputImage)
 %function gets a selected frame of a video, finds the position of the
 %brand and returns a cropped image of the logo
 %  you can find details of each function int the function itself
 
-%just to test the frame(image) will be initializied here
+%just to test, the frame(image) will be initializied here
+%inputImage = imread('toyota.png');
 %inputImage = imread('suzuki.png');
-inputImage = imread('skoda.jpg');
 
 %image resized on 480 rows and imresize calculates automatically the colums
-image = imresize(inputImage, [480 NaN]);
+inputImage = imresize(inputImage, [480 NaN]);
 
 %image to gray image
-imgray = RGB2Grey(image);
-
-%image to binary image
-imbin = Grey2Binary(imgray);
+imgray = RGB2Grey(inputImage);
 
 %filter edges with sobel filter
-image = edge(imgray, 'Sobel');
-%image = Grey2Sobel(imgray);
+%image = edge(imgray, 'Sobel');
+image = Grey2Canny(imgray);
 
-%highlites the edges
-image = imDilation(image, strEL(2), 2);
+%highlight the edges
+image = imDilation(image, strEL(1), 1);
 
 %fills closed surfaces
-image = imfill(image, 'holes');%füllt flächen aus
+image = imfill(image, 'holes');
 
 %reduces the surfaces
-%im = imerode(im, strel('diamond', 10)); % löscht unnötige weiße flächen weg
-image = imErosion(image);
+%image = imerode(image, strel('diamond', 10));
+image = imErosion(image, 10);
 
 %highlites the edges
 image = imDilation(image, strEL(6), 6);
@@ -39,10 +36,15 @@ image = imDilation(image, strEL(6), 6);
 %returns the boundingBox of the logo
 boundingBox = regionProps(image);
 
-%cuts the boundingBox out of the binary image
-croppedImage = imcrop(imbin, boundingBox);
+%cuts the boundingBox out of the input image
+croppedImage = imCrop(imgray, boundingBox);
+
+%image resized on 480 rows and imresize calculates automatically the colums
+croppedImage = imresize(croppedImage, [480 NaN]);
+
+%image to binary image
+croppedImage = Grey2Binary(croppedImage);
 
 %for testing show the croppped logo
 imshow(croppedImage);
 end
-
