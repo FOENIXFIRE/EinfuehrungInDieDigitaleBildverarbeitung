317e0c67fc5e7e4bef84745b45adb69c6e46a300
Vincent Zöchling <vinc.z@gmx.at>
2021-01-07 20:56:38 +0100

    Changed: carCropping

--- carCropping.m
+++ carCropping.m
@@ -1,26 +1,21 @@
 %Vincent Zoechling 11913652
 function [outputImage] = carCropping(inputImage, bboxAuto)
 
-%
+%Resizing the bounding box
 scale = 5;
 bbox = bboxAuto;
 bboxResized = bboxresize(bbox,scale);
 
-% imgGrey = RGB2Grey(inputImage);
-% imgFilter = medfilt2(imgGrey);
-% imgFilter2 = medfilt2(imgFilter);
-% imgFilter3 = medfilt2(imgFilter2)
-% imgCanny = Grey2Canny(imgFilter3);
 
-
+%Drawing the bounding box
 annotatedImage = insertShape(inputImage,"rectangle",bboxResized,"LineWidth",5);
 imshow(annotatedImage)
 title('Resized Image and Bounding Box')
 
+
+%Cropping the image
 outputImage = imcrop(annotatedImage, bboxResized);
 imshow(outputImage)
-
-
 
 
 end
